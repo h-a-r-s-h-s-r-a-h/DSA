aaa9c6cde71b500274abd83c245d6917ba8cf17f
Harsh <harshvirat894@gmail.com>
2025-04-11 00:47:53 +0530

    DSA

--- Code/Array/Project06/Main.java
+++ Code/Array/Project06/Main.java
@@ -13,7 +13,7 @@ public class Main {
 
     public static void main(String[] args) {
         Main myObj = new Main();
-        int[] arr = {1,2,3,4,65,2,5,322};
+        int[] arr = {1, 2, 3, 4, 65, 2, 5, 322};
         int max = myObj.findMax(arr);
         System.out.println("Maximum value :- " + max);
     }
