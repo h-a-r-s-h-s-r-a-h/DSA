e5add0d9d9cb49d5655d7d09d597148c8a0f3f28
Harsh <harshvirat894@gmail.com>
2025-04-11 00:47:36 +0530

    DSA

--- Code/Array/Project04/Main.java
+++ Code/Array/Project04/Main.java
@@ -3,18 +3,19 @@ package Code.Array.Project04;
 import java.util.Scanner;
 
 public class Main {
-    public int findMinimum(int[] arr){
-        if(arr == null || arr.length == 0){
+    public int findMinimum(int[] arr) {
+        if (arr == null || arr.length == 0) {
             throw new IllegalArgumentException("Invalid input");
         }
         int min = arr[0];
-        for(int i = 1; i < arr.length; i++){
-            if(min > arr[i]){
+        for (int i = 1; i < arr.length; i++) {
+            if (min > arr[i]) {
                 min = arr[i];
             }
         }
         return min;
     }
+
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         Main myObj = new Main();
@@ -22,7 +23,7 @@ public class Main {
         int len = input.nextInt();
         int[] arr = new int[len];
         System.out.println("Enter the elements of array :- ");
-        for(int i = 0; i < len; i++){
+        for (int i = 0; i < len; i++) {
             arr[i] = input.nextInt();
         }
         int minimum = myObj.findMinimum(arr);
