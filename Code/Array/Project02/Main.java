a5f8faffd55b6173d8014e0ac3e0280dae34813e
Harsh <harshvirat894@gmail.com>
2025-04-11 00:47:11 +0530

    DSA

--- Code/Array/Project02/Main.java
+++ Code/Array/Project02/Main.java
@@ -3,25 +3,26 @@ package Code.Array.Project02;
 import java.util.Scanner;
 
 public class Main {
-    public int[] removeEven(int[] arr){
+    public int[] removeEven(int[] arr) {
         int oddCount = 0;
-        for(int i=0;i<arr.length;i++){
-            if(arr[i] % 2 != 0){
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] % 2 != 0) {
                 oddCount++;
             }
         }
         int[] result = new int[oddCount];
         int idx = 0;
-        for(int i=0;i<arr.length;i++){
-            if(arr[i] % 2 != 0){
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] % 2 != 0) {
                 result[idx] = arr[i];
                 idx++;
             }
         }
         return result;
     }
-    public void printArray(int[] arr){
-        for(int i=0;i<arr.length;i++){
+
+    public void printArray(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + " ");
         }
         System.out.println();
@@ -34,7 +35,7 @@ public class Main {
         int length = input.nextInt();
         int[] myArr = new int[length];
         System.out.println("Enter the elements of Array :- ");
-        for(int i=0;i<length;i++){
+        for (int i = 0; i < length; i++) {
             myArr[i] = input.nextInt();
         }
         System.out.println("Printing original Array :- ");
