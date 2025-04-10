937fdbad85a6b40195212baf30fab8d2f973a148
Harsh <harshvirat894@gmail.com>
2025-04-11 00:48:22 +0530

    DSA

--- Code/Array/Project09/Main.java
+++ Code/Array/Project09/Main.java
@@ -1,10 +1,10 @@
 package Code.Array.Project09;
 
 public class Main {
-    public int findMissingNumber(int[] arr){
+    public int findMissingNumber(int[] arr) {
         int n = arr.length + 1;
-        int sum = n * (n + 1)/2;
-        for(int i = 0; i < arr.length; i++){
+        int sum = n * (n + 1) / 2;
+        for (int i = 0; i < arr.length; i++) {
             sum = sum - arr[i];
         }
         return sum;
@@ -12,7 +12,7 @@ public class Main {
 
     public static void main(String[] args) {
         Main myObj = new Main();
-        int[] arr = {2,4,1,8,6,3,7};
+        int[] arr = {2, 4, 1, 8, 6, 3, 7};
         int missingNumber = myObj.findMissingNumber(arr);
         System.out.println("Missing Number :- " + missingNumber);
     }
